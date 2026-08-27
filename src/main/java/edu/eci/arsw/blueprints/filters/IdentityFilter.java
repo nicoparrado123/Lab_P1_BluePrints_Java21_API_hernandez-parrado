242073152bc001e23ed59bec04e9135acd7fa316
juanhernandez2003 <juan.hmoreno@mail.escuelaing.edu.co>
2026-08-27 07:17:47 -0500

    fix: Arreglo punto 1

--- src/main/java/edu/eci/arsw/blueprints/filters/IdentityFilter.java
+++ src/main/java/edu/eci/arsw/blueprints/filters/IdentityFilter.java
@@ -1,9 +1,11 @@
 package edu.eci.arsw.blueprints.filters;
 
 import edu.eci.arsw.blueprints.model.Blueprint;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("!redundancy & !undersampling")
 public class IdentityFilter implements BlueprintsFilter {
     @Override
     public Blueprint apply(Blueprint bp) { return bp; }
